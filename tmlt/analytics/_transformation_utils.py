37df3ff8c786425f2217055d27ffbd35a69a81c4
Amritha Pai <amritha.pai@tmlt.io>
2023-02-02 21:44:30 +0000

    Transformation utilities
    tumult#2405

--- tmlt/analytics/_transformation_utils.py
+++ tmlt/analytics/_transformation_utils.py
@@ -1,17 +1,40 @@
 """Useful functions to be used with transfomations."""
+
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Tumult Labs 2023
 
-from typing import Callable, Dict, Tuple, Type
+# pylint: disable=unused-argument
+
+from typing import Callable, Dict, Optional, Tuple, Type, cast
 
 from tmlt.analytics._table_identifier import Identifier, TemporaryTable
 from tmlt.analytics._table_reference import TableReference, lookup_domain, lookup_metric
 from tmlt.core.domains.base import Domain
 from tmlt.core.domains.collections import DictDomain
+from tmlt.core.domains.spark_domains import SparkDataFrameDomain
 from tmlt.core.metrics import AddRemoveKeys, DictMetric, Metric
 from tmlt.core.transformations.base import Transformation
-from tmlt.core.transformations.dictionary import GetValue as GetValueTransformation
-from tmlt.core.transformations.dictionary import create_transform_value
+from tmlt.core.transformations.dictionary import Subset as SubsetTransformation
+from tmlt.core.transformations.dictionary import (
+    create_copy_and_transform_value,
+    create_rename,
+    create_transform_value,
+)
+from tmlt.core.transformations.spark_transformations.add_remove_keys import (
+    PersistValue as PersistValueTransformation,
+)
+from tmlt.core.transformations.spark_transformations.add_remove_keys import (
+    RenameValue as RenameValueTransformation,
+)
+from tmlt.core.transformations.spark_transformations.add_remove_keys import (
+    UnpersistValue as UnpersistValueTransformation,
+)
+from tmlt.core.transformations.spark_transformations.persist import (
+    Persist as PersistTransformation,
+)
+from tmlt.core.transformations.spark_transformations.persist import (
+    Unpersist as UnpersistTransformation,
+)
 
 
 def generate_nested_transformation(
@@ -20,6 +43,7 @@ def generate_nested_transformation(
     generator_dict: Dict[
         Type[Metric], Callable[[Domain, Metric, Identifier], Transformation]
     ],
+    table_identifier: Optional[Identifier] = None,
 ) -> Tuple[Transformation, TableReference]:
     """Generate a nested transformation.
 
@@ -36,11 +60,13 @@ def generate_nested_transformation(
           where generator() is a function which takes the associated domain and metric
           of the parent_reference and generates a transormation using the
           Metric, e.g. generator(parent_domain, parent_metric, target_identifier)
+        table_identifier: identifier for new table in case of rename and original
+          table in case of delete/persist/unpersist
     """
     parent_domain = lookup_domain(base_transformation.output_domain, parent_reference)
     parent_metric = lookup_metric(base_transformation.output_metric, parent_reference)
 
-    target_table = TemporaryTable()
+    target_table = TemporaryTable() if table_identifier is None else table_identifier
 
     try:
         gen_transformation = generator_dict[type(parent_metric)]
@@ -74,23 +100,131 @@ def generate_nested_transformation(
     return base_transformation | transformation, parent_reference / target_table
 
 
-def get_table_from_ref(
-    transformation: Transformation, ref: TableReference
-) -> Transformation:
-    """Returns a GetValue transformation, finding the referenced table."""
-    for p in ref.path:
-        domain = transformation.output_domain
-        metric = transformation.output_metric
-        if not isinstance(domain, DictDomain):
-            raise ValueError(
-                "Invalid transformation domain. This is probably a bug; please let us"
-                " know about it so we can fix it!"
-            )
-        if not isinstance(metric, (DictMetric, AddRemoveKeys)):
-            raise ValueError(
-                "Invalid transformation domain. This is probably a bug; please let us"
-                " know about it so we can fix it!"
-            )
+def rename_table(
+    base_transformation: Transformation,
+    base_ref: TableReference,
+    new_table_id: Identifier,
+) -> Tuple[Transformation, TableReference]:
+    """Renames tables."""
 
-        transformation = transformation | GetValueTransformation(domain, metric, p)
-    return transformation
+    def gen_transformation_dictmetric(pd, pm, tgt):
+        assert isinstance(pd, DictDomain)
+        assert isinstance(pm, DictMetric)
+        return create_rename(pd, pm, base_ref.identifier, tgt)
+
+    def gen_transformation_ark(pd, pm, tgt):
+        assert isinstance(pd, DictDomain)
+        assert isinstance(pm, AddRemoveKeys)
+        # Note: No dataframe column is getting renamed here, RenameValue
+        # is used to rename tables
+        return RenameValueTransformation(pd, pm, base_ref.identifier, tgt, {})
+
+    transformation_generators: Dict[Type[Metric], Callable] = {
+        DictMetric: gen_transformation_dictmetric,
+        AddRemoveKeys: gen_transformation_ark,
+    }
+    new_transformation, new_ref = generate_nested_transformation(
+        base_transformation, base_ref.parent, transformation_generators, new_table_id
+    )
+    return new_transformation, new_ref
+
+
+def delete_table(
+    base_transformation: Transformation, base_ref: TableReference
+) -> Transformation:
+    """Deletes tables."""
+
+    def gen_transformation(pd, pm, tgt):
+        assert isinstance(pd, DictDomain)
+        assert isinstance(pm, (DictMetric, AddRemoveKeys))
+        return SubsetTransformation(
+            pd, pm, list(set(pd.key_to_domain.keys()) - {base_ref.identifier})
+        )
+
+    transformation_generators: Dict[Type[Metric], Callable] = {
+        DictMetric: gen_transformation,
+        AddRemoveKeys: gen_transformation,
+    }
+    new_transformation, _ = generate_nested_transformation(
+        base_transformation,
+        base_ref.parent,
+        transformation_generators,
+        base_ref.identifier,
+    )
+    return new_transformation
+
+
+def persist_table(
+    base_transformation: Transformation,
+    base_ref: TableReference,
+    new_table_id: Identifier,
+) -> Tuple[Transformation, TableReference]:
+    """Persists tables."""
+
+    def gen_transformation_dictmetric(pd, pm, tgt):
+        assert isinstance(pd, DictDomain)
+        assert isinstance(pm, DictMetric)
+        return create_copy_and_transform_value(
+            pd,
+            pm,
+            base_ref.identifier,
+            tgt,
+            PersistTransformation(
+                domain=cast(
+                    SparkDataFrameDomain, pd.key_to_domain[base_ref.identifier]
+                ),
+                metric=pm.key_to_metric[base_ref.identifier],
+            ),
+            lambda *args: None,
+        )
+
+    def gen_transformation_ark(pd, pm, tgt):
+        assert isinstance(pd, DictDomain)
+        assert isinstance(pm, AddRemoveKeys)
+        return PersistValueTransformation(pd, pm, base_ref.identifier, tgt)
+
+    transformation_generators: Dict[Type[Metric], Callable] = {
+        DictMetric: gen_transformation_dictmetric,
+        AddRemoveKeys: gen_transformation_ark,
+    }
+    new_transformation, new_ref = generate_nested_transformation(
+        base_transformation, base_ref.parent, transformation_generators, new_table_id
+    )
+    return new_transformation, new_ref
+
+
+def unpersist_table(
+    base_transformation: Transformation, base_ref: TableReference
+) -> Transformation:
+    """Unpersists tables."""
+
+    def gen_transformation_dictmetric(pd, pm, tgt):
+        assert isinstance(pd, DictDomain)
+        assert isinstance(pm, DictMetric)
+        return create_copy_and_transform_value(
+            pd,
+            pm,
+            base_ref.identifier,
+            tgt,
+            UnpersistTransformation(
+                domain=cast(
+                    SparkDataFrameDomain, pd.key_to_domain[base_ref.identifier]
+                ),
+                metric=pm.key_to_metric[base_ref.identifier],
+            ),
+            lambda *args: None,
+        )
+
+    def gen_transformation_ark(pd, pm, tgt):
+        assert isinstance(pd, DictDomain)
+        assert isinstance(pm, AddRemoveKeys)
+        return UnpersistValueTransformation(pd, pm, base_ref.identifier, tgt)
+
+    transformation_generators: Dict[Type[Metric], Callable] = {
+        DictMetric: gen_transformation_dictmetric,
+        AddRemoveKeys: gen_transformation_ark,
+    }
+    new_transformation, _ = generate_nested_transformation(
+        base_transformation, base_ref.parent, transformation_generators
+    )
+    return new_transformation
