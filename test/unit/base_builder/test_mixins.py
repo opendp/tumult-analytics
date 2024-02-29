b0efd86847ae49e93e98f69006e214c0703afb19
Pira Limpiti <pira.limpiti@tmlt.io>
2024-02-29 22:31:08 +0000

    Base Builder
    Refactoring how the builders work, by separating out each component as Mixins.
    
    See merge request tumult-labs/analytics-upstream!36
    
    Upstream-hash: 76048672b885f038799626c498ff64cae2771d82

---
+++ test/unit/base_builder/test_mixins.py
@@ -0,0 +1,170 @@
+"""Tests for builder mixins."""
+
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Tumult Labs 2024
+
+import pandas as pd
+import pytest
+from pyspark.sql.types import DoubleType, LongType, StructField, StructType
+
+from tmlt.analytics._base_builder import (
+    BaseBuilder,
+    DataframeMixin,
+    ParameterMixin,
+    PrivacyBudgetMixin,
+)
+from tmlt.analytics.privacy_budget import PureDPBudget
+from tmlt.analytics.protected_change import AddMaxRows, AddRowsWithID
+
+
+class _PrivacyBudgetBuilder(PrivacyBudgetMixin, BaseBuilder):
+    def build(self):
+        return self._privacy_budget
+
+
+def test_privacy_budget():
+    """PrivacyBudgetMixin works correctly."""
+    budget = PureDPBudget(1)
+    assert _PrivacyBudgetBuilder().with_privacy_budget(budget).build() == budget
+
+
+def test_privacy_budget_not_set():
+    """PrivacyBudgetMixin raises an exception when budget is not set."""
+    with pytest.raises(ValueError):
+        _PrivacyBudgetBuilder().build()
+
+
+def test_privacy_budget_multiple_set():
+    """PrivacyBudgetMixin raises an exception when budget is set multiple times."""
+    budget = PureDPBudget(1)
+    builder = _PrivacyBudgetBuilder().with_privacy_budget(budget)
+    with pytest.raises(ValueError):
+        builder.with_privacy_budget(budget)
+
+
+class _DataframeBuilder(DataframeMixin, BaseBuilder):
+    def build(self):
+        return self._private_dataframes, self._public_dataframes, self._id_spaces
+
+
+def test_dataframes(spark):
+    """DataframeMixin works correctly."""
+    df1 = spark.createDataFrame(pd.DataFrame({"A": [1]}))
+    df2 = spark.createDataFrame(pd.DataFrame({"A": [2.0]}))
+    df3 = spark.createDataFrame(pd.DataFrame({"A": [3]}))
+
+    private_dfs, public_dfs, id_spaces = (
+        _DataframeBuilder()
+        .with_id_space("id1")
+        .with_id_space("df3")  # Make sure id spaces don't conflict with dataframes
+        .with_private_dataframe("df1", df1, AddRowsWithID("A"))
+        .with_private_dataframe("df2", df2, AddMaxRows(5))
+        .with_public_dataframe("df3", df3)
+        .build()
+    )
+    assert set(private_dfs.keys()) == {"df1", "df2"}
+    assert private_dfs["df1"][0].head()["A"] == 1
+    assert private_dfs["df1"][1] == AddRowsWithID("A")
+    assert private_dfs["df1"][0].schema == StructType(
+        [StructField("A", LongType(), True)]
+    )
+
+    assert private_dfs["df2"][0].head()["A"] == 2.0
+    assert private_dfs["df2"][1] == AddMaxRows(5)
+    assert private_dfs["df2"][0].schema == StructType(
+        [StructField("A", DoubleType(), True)]
+    )
+
+    assert set(public_dfs.keys()) == {"df3"}
+    assert public_dfs["df3"].head()["A"] == 3
+    assert public_dfs["df3"].schema == StructType([StructField("A", LongType(), True)])
+
+    assert id_spaces == {"id1", "df3"}
+
+
+def test_dataframes_invalid_ids(spark):
+    """DataframeMixin rejects invalid IDs."""
+    df = spark.createDataFrame(pd.DataFrame({"A": [1]}))
+
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_private_dataframe("1st", df, AddMaxRows(1))
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_private_dataframe("test-id", df, AddMaxRows(1))
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_public_dataframe("1st", df)
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_public_dataframe("test-id", df)
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_id_space("1st")
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_id_space("test-id")
+
+
+def test_dataframes_name_conflicts(spark):
+    """DataframeMixin rejects duplicate IDs."""
+    df = spark.createDataFrame(pd.DataFrame({"A": [1]}))
+
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_private_dataframe(
+            "t1", df, AddMaxRows(1)
+        ).with_private_dataframe("t1", df, AddMaxRows(1))
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_public_dataframe("t1", df).with_private_dataframe(
+            "t1", df, AddMaxRows(1)
+        )
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_private_dataframe(
+            "t1", df, AddMaxRows(1)
+        ).with_public_dataframe("t1", df)
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_id_space("id1").with_id_space("id2").with_id_space(
+            "id1"
+        )
+
+
+def test_dataframes_empty_column_name(spark):
+    """DataframeMixin rejects dataframes with empty column names."""
+    df = spark.createDataFrame(pd.DataFrame({"": [1]}))
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_private_dataframe("t1", df, AddMaxRows(1))
+
+
+@pytest.mark.parametrize(
+    "dataframe",
+    [
+        pd.DataFrame({"A": [bytearray(b"x")]}),
+        pd.DataFrame({"A": [{"x": 1}]}),
+    ],
+)
+def test_dataframes_invalid_schemas(spark, dataframe):
+    """DataframeMixin rejects dataframes with unsupported schemas."""
+    df = spark.createDataFrame(dataframe)
+    with pytest.raises(ValueError):
+        _DataframeBuilder().with_private_dataframe("t1", df, AddMaxRows(1))
+
+
+class _ParameterBuilder(ParameterMixin, BaseBuilder):
+    def build(self):
+        return self._parameters
+
+
+def test_parameters():
+    """ParameterMixin works correctly."""
+    parameters = (
+        _ParameterBuilder()
+        .with_parameter("a", 1)
+        .with_parameter("b", "x")
+        .with_parameter("c", {1: 2, 3: 4})
+        .build()
+    )
+    assert parameters["a"] == 1
+    assert parameters["b"] == "x"
+    assert parameters["c"] == {1: 2, 3: 4}
+
+
+def test_parameters_name_conflicts():
+    """ParameterMixin rejects duplicate parameters."""
+    builder = _ParameterBuilder().with_parameter("a", 1)
+    with pytest.raises(ValueError):
+        builder.with_parameter("a", 2)
+    assert builder.build()["a"] == 1
---
+++ tmlt/analytics/_base_builder.py
@@ -0,0 +1,160 @@
+"""Building blocks of modular builders used by various Analytics objects."""
+
+# SPDX-License-Identifier: Apache-2.0
+# Copyright Tumult Labs 2024
+
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Optional, Set, Tuple
+
+from pyspark.sql import DataFrame
+
+from tmlt.analytics._coerce_spark_schema import coerce_spark_schema_or_fail
+from tmlt.analytics.privacy_budget import PrivacyBudget
+from tmlt.analytics.protected_change import ProtectedChange
+
+
+class BaseBuilder(ABC):
+    """A base for various builders of privacy-tracking objects."""
+
+    @abstractmethod
+    def build(self) -> Any:
+        """Construct the type that this builder builds."""
+
+
+class PrivacyBudgetMixin:
+    """Add support for setting the privacy budget for a builder."""
+
+    __budget: Optional[PrivacyBudget] = None
+
+    def __init__(self):
+        """Constructor."""
+        super().__init__()
+        self.__budget = None
+
+    def with_privacy_budget(self, privacy_budget: PrivacyBudget):
+        """Set the privacy budget for the object being built."""
+        if self.__budget is not None:
+            raise ValueError("This builder already has a privacy budget set")
+        self.__budget = privacy_budget
+        return self
+
+    @property
+    def _privacy_budget(self) -> PrivacyBudget:
+        if self.__budget is None:
+            raise ValueError("This builder must have a privacy budget set")
+        return self.__budget
+
+
+class DataframeMixin:
+    """Add private and public dataframe support to a builder."""
+
+    __private_dataframes: Dict[str, Tuple[DataFrame, ProtectedChange]]
+    __public_dataframes: Dict[str, DataFrame]
+    __id_spaces: Set[str]
+
+    def __init__(self):
+        """Constructor."""
+        super().__init__()
+        self.__private_dataframes = {}
+        self.__public_dataframes = {}
+        self.__id_spaces = set()
+
+    def with_private_dataframe(
+        self, source_id: str, dataframe: DataFrame, protected_change: ProtectedChange
+    ):
+        """Adds a Spark DataFrame as a private source.
+
+        Not all Spark column types are supported in private sources; see
+        :data:`tmlt.analytics.session.SUPPORTED_SPARK_TYPES` for information about
+        which types are supported.
+
+        Args:
+            source_id: Source id for the private source dataframe.
+            dataframe: Private source dataframe to perform queries on,
+                corresponding to the ``source_id``.
+            protected_change: A
+                :class:`~tmlt.analytics.protected_change.ProtectedChange`
+                specifying what changes to the input data should be protected.
+        """
+        _assert_is_identifier(source_id)
+        if (
+            source_id in self.__private_dataframes
+            or source_id in self.__public_dataframes
+        ):
+            raise ValueError(f"Table '{source_id}' already exists")
+
+        dataframe = coerce_spark_schema_or_fail(dataframe)
+        self.__private_dataframes[source_id] = (dataframe, protected_change)
+        return self
+
+    def with_public_dataframe(self, source_id: str, dataframe: DataFrame):
+        """Add a public dataframe."""
+        _assert_is_identifier(source_id)
+        if (
+            source_id in self.__private_dataframes
+            or source_id in self.__public_dataframes
+        ):
+            raise ValueError(f"Table '{source_id}' already exists")
+
+        dataframe = coerce_spark_schema_or_fail(dataframe)
+        self.__public_dataframes[source_id] = dataframe
+        return self
+
+    def with_id_space(self, id_space: str):
+        """Add an identifier space.
+
+        This defines a space of identifiers that map 1-to-1 to the identifiers
+        being protected by a table with the :class`~AddRowsWithID` protected
+        change. Any table with such a protected change must be a member of some
+        identifier space.
+        """
+        _assert_is_identifier(id_space)
+        if id_space in self.__id_spaces:
+            raise ValueError(f"ID space '{id_space}' already exists")
+        self.__id_spaces.add(id_space)
+        return self
+
+    @property
+    def _private_dataframes(self) -> Dict[str, Tuple[DataFrame, ProtectedChange]]:
+        if not self.__private_dataframes:
+            raise ValueError("At least one private dataframe must be specified")
+        return dict(self.__private_dataframes)
+
+    @property
+    def _public_dataframes(self) -> Dict[str, DataFrame]:
+        return dict(self.__public_dataframes)
+
+    @property
+    def _id_spaces(self) -> Set[str]:
+        return self.__id_spaces
+
+
+class ParameterMixin:
+    """Add support for setting parameters to a builder."""
+
+    __parameters: Dict[str, Any]
+
+    def __init__(self):
+        """Constructor."""
+        super().__init__()
+        self.__parameters = {}
+
+    def with_parameter(self, name: str, value: Any):
+        """Set the value of a parameter."""
+        if name in self.__parameters:
+            raise ValueError(f"Parameter '{name}' has already been set")
+        self.__parameters[name] = value
+        return self
+
+    @property
+    def _parameters(self) -> Dict[str, Any]:
+        return dict(self.__parameters)
+
+
+def _assert_is_identifier(identifier: str):
+    """Check that the given ``identifier`` is a valid table name."""
+    if not identifier.isidentifier():
+        raise ValueError(
+            "Names must be valid Python identifiers: they can only contain "
+            "alphanumeric characters and underscores, and cannot begin with a number."
+        )
