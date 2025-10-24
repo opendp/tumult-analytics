1946e8d12548f629fdcd0aeb6471bb4a534f3202
Damien Desfontaines <TedTed@users.noreply.github.com*>
2025-10-24 16:22:58 +0200

    change some tests

--- test/system/session/rows/conftest.py
+++ test/system/session/rows/conftest.py
@@ -82,7 +82,7 @@ EVALUATE_TESTS = [
         pd.DataFrame({"total": [4]}),
     ),
     (  # Total with LAPLACE (Geometric noise gets applied)
-        QueryBuilder("private").count(name="total", mechanism=CountMechanism.LAPLACE),
+        QueryBuilder("private").count(name="total", mechanism="laplace"),
         GroupByCount(
             child=PrivateSource("private"),
             groupby_keys=KeySet.from_dict({}),
@@ -93,7 +93,7 @@ EVALUATE_TESTS = [
     ),
     (  # Total with LAPLACE (Geometric noise gets applied)
         QueryBuilder("private").count_distinct(
-            name="total", mechanism=CountDistinctMechanism.LAPLACE
+            name="total", mechanism="laplace",
         ),
         GroupByCountDistinct(
             child=PrivateSource("private"),
