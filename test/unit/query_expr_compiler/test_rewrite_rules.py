7651dc3a8de2b1cc97a36bc00288dc907994cc05
Tom Magerlein <dev@tom.magerlein.com>
2026-07-04 10:40:40 -0400

    Add QueryExpr.constraints method for constraint propagation
    Adds a new QueryExpr.constraints method taking a session Catalog and computing
    the constraints on the output of that query expression. This method should have
    identical behavior to the existing constraint propagation logic used in the
    transformation visitor.
    
    In this commit, the new method is only compared against the existing propagation
    logic, it does not replace it.

--- test/unit/query_expr_compiler/test_rewrite_rules.py
+++ test/unit/query_expr_compiler/test_rewrite_rules.py
@@ -1,7 +1,7 @@
 """Tests for rewrite rules."""
 
 from dataclasses import dataclass, replace
-from typing import Any, Union
+from typing import Any, FrozenSet, List, Union
 
 import pytest
 from tmlt.core.measurements.aggregations import NoiseMechanism
@@ -43,6 +43,7 @@ from tmlt.analytics._query_expr_compiler._rewrite_rules._utils import (
     CompilationInfo,
 )
 from tmlt.analytics._schema import ColumnDescriptor, ColumnType, FrozenDict, Schema
+from tmlt.analytics.constraints import Constraint
 
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Tumult Labs 2025
@@ -362,6 +363,10 @@ class SomeKindOfPostProcessing(SingleChildQueryExpr):
     def schema(self, catalog: Catalog) -> Schema:
         """Just propagate the schema from the child."""
         return self.child.schema(catalog)
+
+    def constraints(self, catalog: Catalog) -> FrozenSet[Constraint]:
+        """Just propagate the constraints from the child."""
+        return self.child.constraints(catalog)
 
     def accept(self, visitor: "QueryExprVisitor") -> Any:
         """This should not be called."""
