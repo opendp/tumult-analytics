a9122cc1435364e82c11a1f5bf45e90304753603
Damien Desfontaines <1112173+TedTed@users.noreply.github.com>
2026-09-10 17:44:36 +0200

    reenable missing docstring linters

--- src/tmlt/analytics/config.py
+++ src/tmlt/analytics/config.py
@@ -114,7 +114,7 @@ class Config:
 
     _instance = None
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs):  # noqa: D102
         # Enforces that Config is a singleton.
         # No docstring to prevent this from showing up in docs.
         if not cls._instance:
